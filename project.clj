981db95f3efcf757e1f500e3f718868c1a25a763
Howard Ding <hding2@brynmawr.edu>
2023-11-29 12:15:32 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.clojars.hading/lein-template.aoc "0.1.1"
+  :description "Leiningen template for my personal Advent of Code projects"
+  :url "https://github.com/hading"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :eval-in-leiningen true)
---
+++ resources/leiningen/new/aoc/core.clj
@@ -0,0 +1,13 @@
+(ns {{namespace}}
+  (:require
+   [had-utils.io :as hio]
+   [had-utils.collections :as hc]
+   [had-utils.core :as hcore]
+   [superstring.core :as str]
+   [clojure.set :as s]))
+
+(def test-input nil)
+(def input nil)
+
+(defn main-1 [])
+(defn main-2 [])
