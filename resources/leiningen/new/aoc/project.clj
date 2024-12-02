69b3982a56c5dcd73501245a1f7c9353fa4a244f
Howard Ding <hding2@brynmawr.edu>
2024-12-02 10:36:57 -0500

    bump versions

--- resources/leiningen/new/aoc/project.clj
+++ resources/leiningen/new/aoc/project.clj
@@ -7,5 +7,5 @@
                  [superstring "3.2.0"]
                  [tupelo "24.11.22"]
                  [ubergraph "0.9.0"]
-                 [org.clojars.hading/had-utils "5.1.2"]]
+                 [org.clojars.hading/had-utils "6.0.0"]]
   :repl-options {:init-ns {{namespace}}})
