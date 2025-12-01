f6b5d27d73c062fae5d7e4dd0b2a086fa28ca79d
Howard Ding <hding2@brynmawr.edu>
2025-12-01 14:58:15 -0500

    update library versions

--- resources/leiningen/new/aoc/project.clj
+++ resources/leiningen/new/aoc/project.clj
@@ -4,8 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.3"]
-                 [superstring "3.2.0"]
-                 [tupelo "24.11.22"]
+                 [superstring "3.2.1"]
+                 [tupelo "25.07.04"]
                  [ubergraph "0.9.0"]
                  [org.clojars.hading/had-utils "7.1.1"]]
   :repl-options {:init-ns {{namespace}}}
